3945195734ab444eb6ec1fb5b3531ffe49eefc0f
Yash <guptayash321@gmail.com>
2019-12-03 17:45:26 -0600

    Confirmed functionality of IFFT'd output after changing sound intensity at various frequencies.

--- fantastic-finale-imbr92/DEVELOPMENT.md
+++ fantastic-finale-imbr92/DEVELOPMENT.md
@@ -1,6 +1,8 @@
 11/16 -- Made basic GUI with ofxGui and hooked up an MP3 file to confirm functionality Play and Load buttons.
 11/17-19 -- Failed at using ofxAudifile, ofxFft and ofxSoundObjects due to lack of documentation and lack of knowledge about threads and DSP.
+
 11/20 -- Made Polynomial.h and Polynomial.cpp to simplify the math involved in the project with abstraction. 
+
 12/1 -- Read articles about the DSP theory behind Equalizers and FFT. Then read documentation of FFTW and libsndfile libraries.
 Links: 
 https://www.meldaproduction.com/tutorials/text/equalizers
@@ -8,9 +10,13 @@ https://www.zytrax.com/tech/audio/equalization.html#fft
 http://fftw.org/fftw3_doc/Complex-One_002dDimensional-DFTs.html#Complex-One_002dDimensional-DFTs
 http://www.fftw.org/~fftw/fftw2.pdf
 http://digitalsoundandmusic.com/5-3-3-reading-and-writing-formatted-audio-files-in-c/
+
 12/2 -- 
 - Went to Office Hours to get help linking DLL libraries to my project. 
 - Implemented wav file reader and performed FFT after each read 
 - Stored relative sound intensities of each frequency at each time point in a 2D vector
 - Performed IFFT and output the result back to a wav file
-- Having issues with RIFF header being different in output and input wav files (Will hopefully resolve in OH tmrw).
+- Having issues with RIFF header being different in output and input wav files
+
+12/3 -- 
+- Rewrote first 200 bytes of binary data in out.wav with info from in.wav (to make RIFF header consistent)
--- fantastic-finale-imbr92/src/main.cpp
+++ fantastic-finale-imbr92/src/main.cpp
@@ -5,8 +5,8 @@
 
 const signed kWidth = 1200;
 const signed kHeight = 800;
-const signed N = 2048;
-#define BUFF_SIZE 2048
+const signed N = 4096;
+#define BUFF_SIZE 4096
 #define MAX_FREQ 22  // KHz
 #define IN_PATH "C:/Users/Yash/Documents/exwav.wav"
 #define OUT_PATH "C:/Users/Yash/Documents/out.wav"
@@ -21,24 +21,27 @@ int main() {
     // pass in width and height too:
     // audiofile.load("C:/Users/Yash/Documents/100Hz.wav");
     WavInFile inFile(IN_PATH);
-    WavOutFile outFile(OUT_PATH, inFile.getNumSamples(), inFile.getBytesPerSample() * 8, inFile.getNumChannels());
-    //outFile.fillInHeader(inFile.getSampleRate(), inFile.getBytesPerSample() * 8, inFile.getNumChannels());
+    WavOutFile outFile(OUT_PATH, inFile.getNumSamples(),
+                       16, inFile.getNumChannels());
+    // outFile.fillInHeader(inFile.getSampleRate(), inFile.getBytesPerSample() *
+    // 8, inFile.getNumChannels());
     float sampleBuffer[BUFF_SIZE];
-    fftw_complex *in, *out;
+    fftw_complex *in, *out, *rev;
     fftw_plan p, q;
     in = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * N);
     out = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * N);
+    rev = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * N);
     p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-    q = fftw_plan_dft_1d(N, in, out, FFTW_BACKWARD, FFTW_ESTIMATE);
-	
-	// sample rate is 44100 ==> Follows Nyquist thm
+    q = fftw_plan_dft_1d(N, out, rev, FFTW_BACKWARD, FFTW_ESTIMATE);
+
+    // sample rate is 44100 ==> Follows Nyquist thm
     // cout << inFile.getSampleRate();
 
-	//goes through entire file and executes each dft
+    // goes through entire file and executes each dft
     // db levels of each sample time
     vector<vector<double>> spdbl;
     double add_factor = 0;
-	while (inFile.eof() == 0) {
+    while (inFile.eof() == 0) {
         size_t samplesRead = inFile.read(sampleBuffer, BUFF_SIZE);
 
         for (int i = 0; i < BUFF_SIZE; i++) {
@@ -47,71 +50,93 @@ int main() {
 
         fftw_execute(p); /* repeat as needed */
 
-		double mag = 0, pos = 0;
+        double mag = 0, pos = 0;
         double cdb = 0;
         vector<double> cur;
-        for (int i = 1; i < BUFF_SIZE - 1; ++i) {
-            //cout << sampleBuffer[i] << " " <<  in[i][0] << "\n";
-			//check it is within normal range of hearing (Within 20kHz)
+         for (int i = 1; i < BUFF_SIZE - 1; ++i) {
+            // cout << sampleBuffer[i] << " " <<  in[i][0] << "\n";
+            // check it is within normal range of hearing (Within 20kHz)
             if (i * inFile.getSampleRate() / BUFF_SIZE >= 20000) break;
-			
-			// Normalizing FFT Output
+
+            // Normalizing FFT Output
             out[i][0] /= N;
             out[i][1] /= N;
-			
-			//mag = re^2 + im^2
-            //cout << out[i][0] * out[i][0] + out[i][1] * out[i][1] << " "; 
+
+            // mag = re^2 + im^2
+            // cout << out[i][0] * out[i][0] + out[i][1] * out[i][1] << " ";
             if (mag < out[i][0] * out[i][0] + out[i][1] * out[i][1]) {
                 mag = out[i][0] * out[i][0] + out[i][1] * out[i][1];
                 pos = i;
-			}
-            cdb += 20 * log10(sqrt(out[i][0] * out[i][0] + out[i][1] * out[i][1]));
-            if (!((i)%(1 << 6))) {
-				cur.push_back(cdb/(1 << 6));
+            }
+            cdb +=
+                20 * log10(sqrt(out[i][0] * out[i][0] + out[i][1] *
+                out[i][1]));
+            if (!((i) % (1 << 6))) {
+                cur.push_back(cdb / (1 << 6));
                 add_factor = min(add_factor, cdb / (1 << 6));
                 cdb = 0;
-			}
+            }
+            out[i][0] *= N;
+            out[i][1] *= N;
+                     if (i > 10 && i < 20) {
+                         double cc =
+                             out[i][0] * out[i][0] + out[i][1] * out[i][1];
+                         cc = 20 * log10(cc);
+                         cc += 50;
+                         cc /= 20;
+                         cc = pow(10, cc);
+                         cc -= out[i][1] * out[i][1];
+                         cc = sqrt(cc);
+                         out[i][0] = cc;
+            }
         }
         spdbl.push_back(cur);
-		
-		//confirmed that IFFT of in + out gives samples back in in[i][0]
+
+        // confirmed that IFFT of in + out gives samples back in in[i][0]
         fftw_execute(q);
         for (int i = 0; i < BUFF_SIZE; ++i) {
-            sampleBuffer[i] = in[i][0];
-		}
+            sampleBuffer[i] = rev[i][0] / N;
+            // cout << rev[i][0]/N << " " << in[i][0] << "\n";
+        }
         outFile.write(sampleBuffer, BUFF_SIZE);
-        //break;
-        //cout << "\n";
-        //cout << mag << " pos : " << pos << "\n";
+        // break;
+        // cout << "\n";
+        // cout << mag << " pos : " << pos << "\n";
     }
-    //for (auto i : spdbl) {
+
+    // for (auto i : spdbl) {
     //    for (auto j : i) {
     //        cout << j - add_factor << " ";
     //    }
     //     cout << "\n";
     //}
 
-	//confirmed through comparison of binary files that the header is not being recreated properly but the rest of the file is
+    // confirmed through comparison of binary files that the header is not being
+    // recreated properly but the rest of the file is
     outFile.close();
     fftw_destroy_plan(p);
     fftw_destroy_plan(q);
     fftw_free(in);
     fftw_free(out);
+    fftw_free(rev);
 
-	//overwrite header (not sure about exact header length, check)
-	fstream ifs(IN_PATH, ios::in | ios::binary);
+        // overwrite header (not sure about exact header length, check)
+    fstream ifs(IN_PATH, ios::in | ios::binary);
     fstream ofs(OUT_PATH,
                 ios::in | ios::out | ios::binary);
-    char x;    
-	char y[200];
+    char x;
+    char y[5000];
+    memset(y, 'a', sizeof(y));
     ofs.seekg(0, ios::beg);
-	for (int i = 0; i < 200; ++i) {
-        ifs.read((&x), 1);
-            y[i] = x;
+    for (int i = 0; i < 5000; ++i) {
+		ifs.read((&x), 1);
+		y[i] = x;
 	}
     cout << y << endl;
     cout << ofs.is_open();
+
     ofs.write(y, sizeof(y));
+    ofs.close();
     ofRunApp(new ofApp());
     return 0;
 }
